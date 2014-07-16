0864f9ea2c7b25d27cca403bb10375a7cec2d6c1
magnificogeo <magnifico.geo@gmail.com>
2014-07-16 23:53:25 +0800

    added directives and templates woohoo.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
 // instantiate an angular object for the main app
 var fbApp = angular.module('fbApp',['ngAnimate']);
 
+/***
+* Nav Bar Controller
+***/
+fbApp.controller("NavBarController", function($scope,$http) {
+
+	$scope.testvar = 'hellolol';
+
+	$scope.showManageContacts = function() {
+		if ( $($('managecontactdirective')[0]).hasClass('ng-hide') ) {
+			$($('managecontactdirective')[0]).removeClass('ng-hide');
+		} else {
+			$($('managecontactdirective')[0]).addClass('ng-hide');
+		}
+	}
+
+
+});
+/***
+* Card Stack Controller
+*/
 fbApp.controller("CardStackController", function($scope,$http) {
 
-	$scope.profile_stack = [];
+	$scope.profile_stack = []; // this array will hold the data
 
 	$scope.clickLater = function() {
 		alert('click later succeed');
@@ -11,5 +31,20 @@ fbApp.controller("CardStackController", function($scope,$http) {
 
 	$scope.clickMeet = function() {
 		alert('click meet succeed');
+	}
+});
+
+
+/**
+* Manage Contact Directive
+*/
+fbApp.directive('managecontactdirective', function() {
+	return {
+		restrict: 'E',
+		scope: true,
+		templateUrl:'templates/manage_contact.html',	
+		controller: function($scope,$http) {
+
+		}
 	}
 });

ab4145c07b5ab35c2bc0ac7f03fc051391c1db32
magnificogeo <magnifico.geo@gmail.com>
2014-07-24 16:56:42 +0800

    Converted first screen to css. 60% done. responsive test now.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,6 @@
 // Instantiate an angular object for the main app
-// Current dependencies: Angular Animate, Angular Route
-var fbApp = angular.module('fbApp',['ngAnimate','ngRoute']); 
-
-
-/***
-* Routes
-***/
-fbApp.config(['ngRoute'], function($routeProvider) {
-	$routeProvider.when('/phones', {
-		templateUrl: 'http://google.com/'
-	});
-});
-
+// Current dependencies: None
+var fbApp = angular.module('fbApp',[]); 
 
 /***
 * Nav Bar Controller
